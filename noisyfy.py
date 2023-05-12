609996e6e7c173269459160d5a27f24b4907303a
Gouranga Bala <111289643+AnGoraGou@users.noreply.github.com>
2023-05-12 18:31:30 +0530

    Create noisyfy.py
    This python file generates a csv file containing labels with various level of label noise (pairwise, symmetric and asymmetric etc.) for the images given for training

---
+++ noisyfy.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import random
+
+#read csv file
+df = pd.read_csv('')
+
+#get the label coloumn
+#generate random ids to add label noise
+
+
+
+pd.write('')
